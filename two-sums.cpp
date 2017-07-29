13c30aa9bde5b9c96140d24174eda3899f66bb5f
Hyojin Yi <hyojinyi@live.ca>
2017-07-28 19:03:23 -0700

    two-sums

---
+++ two-sums.cpp
@@ -0,0 +1,35 @@
+/**
+Leetcode
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+class Solution_bruteForce {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < (nums.size()-1); i++ ){
+            for (int j = (i+1); j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result;
+                }
+            }
+        }
+    }
+};
+
+class Solution_OnePassHash {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
