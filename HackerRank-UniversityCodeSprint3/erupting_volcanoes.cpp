2559d0c2739c1490bcbfffaf6bab38a9485a81e2
Hyojin Yi <hyojinyi@live.ca>
2017-10-23 17:03:37 -0700

    init erupting volcanoes

---
+++ HackerRank-UniversityCodeSprint3/erupting_volcanoes.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int findMax(vector<vector<int> > board, int row, int column){
+    int local_max = 0;
+
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++){
+            if (board[i][j] > local_max){
+                local_max = board[i][j];
+            };
+        };
+    };
+    return local_max;
+};
+
+void printBoard(vector<vector<int> > board, int row, int column){
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < column; j++){
+            cout << board[i][j] << " ";
+        };
+        cout << endl;
+    };
+};
+
+int main(){
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    // initialize a n * n board
+    vector<vector<int> > board;
+    board.resize(n);
+    for (int column = 0; column < n; column++){
+        board[column].resize(n);
+    }; 
+
+    for (int a0 = 0; a0 < m; a0++){
+        int x;
+        int y;
+        int w;
+        cin >> x >> y >> w;
+
+        for (int row = 0; row < n; row++){
+            for (int column = 0; column < n; column++){
+                int x_dist = abs(row - x);
+                int y_dist = abs(column -y);
+                board[row][column] =  board[row][column] + w - max(x_dist, y_dist);
+                if (board[row][column] < 0){
+                    board[row][column] = 0;
+                };
+            };
+        };
+        printBoard(board, n, n);
+    }
+    
+    // printBoard(board, n, n);
+
+    // int ans = findMax(board, n, n);
+
+    // cout << ans << endl;
+
+    return 0;
+}
