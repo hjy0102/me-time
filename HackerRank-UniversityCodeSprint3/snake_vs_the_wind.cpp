dbaf091b3f97960a221dcb2cddd9142a8ca5e5ca
Hyojin Yi <hyojinyi@live.ca>
2017-10-26 14:26:53 -0700

    snake vs the wind

--- HackerRank-UniversityCodeSprint3/snake_vs_the_wind.cpp
+++ HackerRank-UniversityCodeSprint3/snake_vs_the_wind.cpp
@@ -11,143 +11,155 @@ void printBoard(vector<vector<int> > board, int row, int column){
     };
 };
 
-vector<vector<int> > transverseBoard(vector<vector<int> > board, int size, int x, int y, char d){
+bool isNorthValid(int size, int x, int y){
+    return x-1 >= 0;
+};
+bool isSouthValid(int size, int x, int y){
+    return x+1 < size;
+};
+bool isEastValid(int size, int x, int y){
+    return y+1 < size;
+}
+bool isWestValid(int size, int x, int y){
+    return y-1 >= 0;
+}
+
+void transverseBoard(vector<vector<int> > board, int size, int x, int y, char d){
     int count = board[x][y];
     int next = count + 1;
-    printBoard(board, size, size);
-    cout << x << " and " << y << endl;
-    cout << "count is : " << count << endl;
-    if (count >= size*size) return board;
-    // check if d is a valid direction to move in
-    switch(d){
-        case 'n': {
-            // move north
-            if ((x-1)>=0){
-                if (board[x-1][y] < count){
-                    cout << "north" <<endl;
-                    board[x-1][y] = next;
-                    transverseBoard(board, size, (x-1), y, d);
-                }
-            }
-            // move east
-            if ((y+1)< size) {
-                if (board[x][y+1] < count) {
-                    cout << "east" <<endl;
-                    board[x][y+1] = next;
-                    transverseBoard(board, size, x, (y+1), d);
-                }
-            }
-            // move west
-            if ((y-1)>=0){
-                if (board[x][y-1] < count) {
-                    cout << "west" << endl;
-                    board[x][y-1] = next;
-                    transverseBoard(board, size, x, (y-1), d);
-                }
-                
-            }
-            // move south
-            if ((x+1)< size){
-                if (board[x+1][y] < count) {
-                    cout << "south" <<endl;
-                    board[x+1][y] = next;
-                    transverseBoard(board, size, (x+1), y, d);
-                };
-            }
-            
-            return board;
-            
-            break;
-        }
-        case 's': {
-            // move south
-            if ((x+1)>=0){
-                cout << "north" <<endl;
-                board[x+1][y] = next;
-                transverseBoard(board, size, (x+1), y, d);
-            // move east
-            } else if ((y+1)< size) {
-                cout << "east" <<endl;
-                board[x][y+1] = next;
-                transverseBoard(board, size, x, (y+1), d);
-            // move west
-            } else if ((y-1)>=0){
-                cout << 'west' << endl;
-                board[x][y-1] = next;
-                transverseBoard(board, size, x, (y-1), d);
-            // move north
-            } else if ((x-1)< size){
-                cout << "south" <<endl;
-                board[x-1][y] = next;
-                transverseBoard(board, size, (x-1), y, d);
-            } else return board;
-            
-            break;
-        }
 
-        case 'e': {
-            // move east
-            if ((y+1)< size) {
-                cout << "east" <<endl;
-                board[x][y+1] = next;
-                transverseBoard(board, size, x, (y+1), d); 
-            }
-            // move north
-            else if ((x-1)< size){
-                    cout << "south" <<endl;
-                    board[x-1][y] = next;
-                    transverseBoard(board, size, (x-1), y, d);
-            }
-            // move south
-            else if ((x+1)>=0){
-                cout << "north" <<endl;
-                board[x+1][y] = next;
-                transverseBoard(board, size, (x+1), y, d);
-            
-            } 
-            // move west 
-            else if ((y-1)>=0){
-                cout << "west" << endl;
-                board[x][y-1] = next;
-                transverseBoard(board, size, x, (y-1), d);
-            }  else return board;
-            
-            break;
-        }
-
-        case 'w': {
-            // move east
-            if ((y-1)< size) {
-                cout << "east" <<endl;
-                board[x][y-1] = next;
-                transverseBoard(board, size, x, (y-1), d); 
-            }
-            // move north
-            else if ((x-1)< size){
-                    cout << "south" <<endl;
-                    board[x-1][y] =next;
-                    transverseBoard(board, size, (x-1), y, d);
-            }
-            // move south
-            else if ((x+1)>=0){
-                cout << "north" <<endl;
-                board[x+1][y] = next;
-                transverseBoard(board, size, (x+1), y, d);
-            
-            } 
-            // move west 
-            else if ((y+1)>=0){
-                cout << 'west' << endl;
-                board[x][y+1] = next;
-                transverseBoard(board, size, x, (y+1), d);
-            }  else return board;
-            
-            break;
-        }
-                                   
+    // cout << x << " and " << y << endl;
+    // cout << "count is : " << count << endl;
+    if (count == size*size){
+        printBoard(board, size, size);
     }
 
-    return board;
+    switch(d){
+        case 'n': {
+            if (isNorthValid(size, x, y)) {
+                if (board[x-1][y] == 0) {
+                    board[x-1][y] = next;
+                    transverseBoard(board, size, x-1, y, d);
+                    break;
+                };
+            };
+            if (isEastValid(size, x, y)) {
+                if (board[x][y+1] == 0) {
+                    board[x][y+1] = next;
+                    transverseBoard(board, size, x, y+1, d);
+                    break;
+                };
+            };
+            if (isWestValid(size, x, y)) {
+                if (board[x][y-1] == 0) {
+                    board[x][y-1] = next;
+                    transverseBoard(board, size, x, y-1, d);
+                    break;
+                };
+            };
+            if (isSouthValid(size, x, y)) {
+                if (board[x+1][y] == 0) {
+                    board[x+1][y] = next;
+                    transverseBoard(board, size, x+1, y, d);
+                    break;
+                };
+            };
+        };
+
+        case 's': {
+            if (isSouthValid(size, x, y)) {
+                if (board[x+1][y] == 0) {
+                    board[x+1][y] = next;
+                    transverseBoard(board, size, x+1, y, d);
+                    break;
+                };
+            };
+            if (isEastValid(size, x, y)) {
+                if (board[x][y+1] == 0) {
+                    board[x][y+1] = next;
+                    transverseBoard(board, size, x, y+1, d);
+                    break;
+                };
+            };
+            if (isWestValid(size, x, y)) {
+                if (board[x][y-1] == 0) {
+                    board[x][y-1] = next;
+                    transverseBoard(board, size, x, y-1, d);
+                    break;
+                };
+            };
+            if (isNorthValid(size, x, y)) {
+                if (board[x-1][y] == 0) {
+                    board[x-1][y] = next;
+                    transverseBoard(board, size, x-1, y, d);
+                    break;
+                };
+            };
+        };
+
+        case 'e': {
+            if (isEastValid(size, x, y)) {
+                if (board[x][y+1] == 0) {
+                    board[x][y+1] = next;
+                    transverseBoard(board, size, x, y+1, d);
+                    break;
+                };
+            };
+            if (isNorthValid(size, x, y)) {
+                if (board[x-1][y] == 0) {
+                    board[x-1][y] = next;
+                    transverseBoard(board, size, x-1, y, d);
+                    break;
+                };
+            };
+            if (isSouthValid(size, x, y)) {
+                if (board[x+1][y] == 0) {
+                    board[x+1][y] = next;
+                    transverseBoard(board, size, x+1, y, d);
+                    break;
+                };
+            };
+            if (isWestValid(size, x, y)) {
+                if (board[x][y-1] == 0) {
+                    board[x][y-1] = next;
+                    transverseBoard(board, size, x, y-1, d);
+                    break;
+                };
+            };
+        };
+
+        case 'w': {
+            if (isWestValid(size, x, y)) {
+                if (board[x][y-1] == 0) {
+                    board[x][y-1] = next;
+                    transverseBoard(board, size, x, y-1, d);
+                    break;
+                };
+            };
+            if (isNorthValid(size, x, y)) {
+                if (board[x-1][y] == 0) {
+                    board[x-1][y] = next;
+                    transverseBoard(board, size, x-1, y, d);
+                    break;
+                };
+            };
+            if (isSouthValid(size, x, y)) {
+                if (board[x+1][y] == 0) {
+                    board[x+1][y] = next;
+                    transverseBoard(board, size, x+1, y, d);
+                    break;
+                };
+            };
+            if (isEastValid(size, x, y)) {
+                if (board[x][y+1] == 0) {
+                    board[x][y+1] = next;
+                    transverseBoard(board, size, x, y+1, d);
+                    break;
+                };
+            };
+        };
+    };
+
 };
 
 int main() {
@@ -167,12 +179,8 @@ int main() {
     };
     
     board[x][y] = 1;
-
-    printBoard(board, n,n);
     
-    vector<vector<int> > ans = transverseBoard(board, n, x, y, d);
-    
-    printBoard(ans, n, n);
+    transverseBoard(board, n, x, y, d);
     
     return 0;
 }
