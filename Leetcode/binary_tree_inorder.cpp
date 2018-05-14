8a2b6e05b08c8582e8df70b4da36d0788ca8fd81
Hyojin Yi <hyojinyi@live.ca>
2018-05-13 22:58:49 -0700

    bt inorder

---
+++ Leetcode/binary_tree_inorder.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> nodes;
+        if (!root) return nodes;
+        
+        stack<TreeNodes*> st;
+        st
+        // check left subtree
+        while(!st.empty()){
+            TreeNode* temp = st.top();
+            st.pop();
+            nodes.push_back(tp)
+        }
+        st.push(root);
+        
+    }
+};
